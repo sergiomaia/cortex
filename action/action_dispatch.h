e25b965303ac6a5efcb51aaf2acdd49650275a40
sergiomaia <sergiomaia@gmail.com>
2026-03-18 12:03:21 -0300

    feat(action): add minimal HTTP server to ActionDispatch

--- action/action_dispatch.h
+++ action/action_dispatch.h
@@ -3,7 +3,11 @@
 
 #include "action_router.h"
 
+/* Dispatch a single already-built request through the router. */
 int action_dispatch(ActionRouter *router, ActionRequest *req, ActionResponse *res);
+
+/* Minimal synchronous HTTP server bound to port 3000. */
+int action_dispatch_serve_http(ActionRouter *router);
 
 #endif /* ACTION_DISPATCH_H */
 
